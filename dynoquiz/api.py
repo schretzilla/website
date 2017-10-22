5e317eee668d995a7a70ff96a1236f56aa5cabae
schretzilla <schretzilla@gmail.com>
2017-10-22 12:44:19 -0700

    removed reference to unused file permissions.py which was causing import issues

--- dynoquiz/api.py
+++ dynoquiz/api.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import BasePermission, SAFE_METHODS, IsAuthenticated
-from permissions import IsOwnerOrReadOnly
+#from permissions import IsOwnerOrReadOnly
 #import pdb; pdb.set_trace()             #FOR TESTING
 
 
