58bad34c8946b86472f5a86cf0195e58f5841ee3
Scott Schretzenmaier <schretzilla@gmail.com>
2018-02-13 20:51:09 -0800

    fixed urls for smart-egg

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^home/$', views.home, name='home'),
     url(r'^sacm/$', views.sacm, name='sacm'),
     url(r'^self-help/$', views.self_help, name='self-help'),
-    url(r'^smart-egg/$', views.smart_egg, name='self-help'),
+    url(r'^smart-egg/$', views.smart_egg, name='smart-egg'),
     
     url(r'^projects/$', views.projects, name='projects'),
     
